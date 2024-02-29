fd42c2cda1728b1262e8118f381f11414fe3c637
Alexander Govorkov <agcentory240@gmail.com>
2024-02-29 20:12:19 +0300

    tests

--- src/app/tabs/authors/authors.page.spec.ts
+++ src/app/tabs/authors/authors.page.spec.ts
@@ -1,17 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AuthorsPage } from './authors.page';
+import { db, IBook, IAuthor } from '../../db';
 
-describe('AuthorsPage', () => {
-  let component: AuthorsPage;
-  let fixture: ComponentFixture<AuthorsPage>;
+describe('AppDB', () => {
+  beforeAll(async () => {
+    await db.authors.clear(); // Очищаем таблицу авторов перед началом тестов
+  });
 
-  beforeEach(async(() => {
-    fixture = TestBed.createComponent(AuthorsPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+  it('test author tables', async () => {
+    // Проверяем, что таблица авторов пуста перед тестом
+    const authorsBefore = await db.authors.toArray();
+    expect(authorsBefore.length).toBe(0);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+    // Вызываем метод populate(), который добавляет данные в базу
+    await db.populate();
+
+    // Проверяем, что таблица авторов содержит авторов после вызова populate()
+    const authorsAfter = await db.authors.toArray();
+    expect(authorsAfter.length).toBeGreaterThan(0);
   });
 });
--- src/app/tabs/books/books.page.spec.ts
+++ src/app/tabs/books/books.page.spec.ts
@@ -1,17 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BooksPage } from './books.page';
+import { db, IBook, IAuthor } from '../../db';
 
-describe('BooksPage', () => {
-  let component: BooksPage;
-  let fixture: ComponentFixture<BooksPage>;
-
-  beforeEach(async(() => {
-    fixture = TestBed.createComponent(BooksPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+describe('AppDB', () => {
+  beforeAll(async () => {
+    await db.books.clear(); // Очищаем таблицу авторов перед началом тестов
   });
+
+  it('test books tables', async () => {
+    // Проверяем, что таблица книг пуста перед тестом
+    const booksBefore = await db.books.toArray();
+    expect(booksBefore.length).toBe(0);
+
+
+    // Вызываем метод populate(), который добавляет данные в базу
+    await db.populate();
+
+    // Проверяем, что таблица книмг содержит авторов после вызова populate()
+    const booksAfter = await db.books.toArray();
+    expect(booksAfter.length).toBeGreaterThan(0);
+  });
+  
 });
