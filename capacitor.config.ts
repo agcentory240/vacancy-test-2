008b204e398a5f8da550fd9f4dc9296dcd309b50
Alexander Govorkov <agcentory240@gmail.com>
2024-02-29 09:47:59 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.vacancy.app2',
+  appName: 'BookList',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
